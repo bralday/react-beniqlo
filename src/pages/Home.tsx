f427be98dcbc96747216b4a7c59fe3d98ed2523c
Benedict Renzon Alday <35050685+bralday@users.noreply.github.com>
2023-01-13 02:28:13 +0800

    Updated Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,19 @@
+import classNames from "../components/Navbar.module.css";
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <div
+      className="mt-4 p-5 bg-primary text-white rounded"
+      style={{
+        backgroundImage: `url(imgs/store2.jpg)`,
+        minHeight: "300px",
+        filter: "brightness(75%)",
+      }}
+    >
+      <h1 className="display-1 ">
+        <strong>Welcome to BeniQlo!</strong>
+      </h1>
+      <p className="lead">Clothing store for men</p>
+    </div>
+  );
 }
